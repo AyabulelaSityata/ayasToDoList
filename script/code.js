492db840665dd8e46dbd2072d299ef8b17367f74
AyabulelaSityata <sityataayabulela@gmail.com>
2023-06-02 16:36:24 +0200

    changes

--- script/code.js
+++ script/code.js
@@ -1,6 +1,28 @@
+const toDOList = document.querySelector('#display')
+const myTasks = document.querySelector('#sort')
+const sortTasks = document.querySelector('#add')
+
 let myArray = [
-    id: ;
-    name: ;
-    createdDate: ;
-    completed: ;
+    {
+        id: 1,
+        name: "exercise",
+        createdDate: ,
+        completed: false,
+    },
+    {
+        id: 2,
+        name: "take a shower",
+        createdDate: ,
+        completed: false,
+    },
+    {
+        id: 3,
+        name: "dress up",
+        createdDate: ,
+        completed: false,
+    },
 ]
+
+function taskOne(){
+
+}
