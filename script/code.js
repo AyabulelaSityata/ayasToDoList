8b6f20ad4c5c3757bfb2bfb55917610997ad2362
sityata ayabulela <ayabulelasityata97@gmail.com>
2023-06-06 00:26:23 +0200

    added changes

--- script/code.js
+++ script/code.js
@@ -1,28 +1,55 @@
-const toDOList = document.querySelector('#display')
-const myTasks = document.querySelector('#sort')
-const sortTasks = document.querySelector('#add')
+let inputDisplay = document.querySelector('#display')
+let sortButton = document.querySelector('#sort-btn')
+let addButton = document.querySelector('#add-btn')
+let listOutput = document.querySelector('.listOutput')
 
-let myArray = [
-    {
-        id: 1,
-        name: "exercise",
-        createdDate: ,
-        completed: false,
-    },
-    {
-        id: 2,
-        name: "take a shower",
-        createdDate: ,
-        completed: false,
-    },
-    {
-        id: 3,
-        name: "dress up",
-        createdDate: ,
-        completed: false,
-    },
-]
+let taskList = JSON.parse('taskList', localStorage.getItem(''))
 
-function taskOne(){
+function addTask(){
+    if( inputDisplay.value === ''){
+        alert("You must enter something!");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputDisplay.value;
+        listOutput.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputDisplay.value = " ";
 
 }
+
+listOutput.addEventListener('click', function(e){
+    if( e.target.tagName === "LI"){
+        e.target.classList.toggle("done");
+    }
+    // else ( e.target.tagName === "SPAN"){
+    //      e.target.parentElement.remove();
+    // }
+}, false);
+
+// let myArray = [
+//     {
+//         id: 1,
+//         name: "exercise",
+//         createdDate: ,
+//         completed: false,
+//     },
+//     {
+//         id: 2,
+//         name: "take a shower",
+//         createdDate: ,
+//         completed: false,
+//     },
+//     {
+//         id: 3,
+//         name: "dress up",
+//         createdDate: ,
+//         completed: false,
+//     },
+// ]
+
+// function taskOne(){
+
+// }
